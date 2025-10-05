2a2e7acd4dd78886049d7baef1b315bb90006a75
costa <viniciuscoscar@gmail.com>
2025-10-05 22:14:24 +0100

    More TODO and less time... Ah...

--- decimal_binary/decimal-binary.cpp
+++ decimal_binary/decimal-binary.cpp
@@ -1,5 +1,9 @@
 /*
  * Base 10 to base 2
+ * TODO: Just base 10 is stupid! Modify it (or create another file, whatever) so
+ * that it converts all letters too, add support for letters. Hexadecimals only
+ * goes until 'F', maybe convert letters to int then convert to binary.
+ * 'Segmentation fault' for input '0', fix this shit
  */
 #include <iostream>
 #include <vector>
@@ -10,13 +14,16 @@ int main(void) {
   // Get base 10 number and store it into a variable
   // TODO: Add type safety for stupid users
   int base10;
-  bool stupid_user;
 
   std::cout << "Enter base 10 number: ";
   std::cin >> base10;
 
   // Vector to store binary digits
   std::vector<int> v_base2;
+  for (int bit; std::cin >> bit;) {
+    std::cin >> base10;
+    base10to2(base10, v_base2);
+  }
 
   // Void function convert base 10 number into base 2
   base10to2(base10, v_base2);
@@ -33,8 +40,13 @@ int main(void) {
 
 /* Void function convert base 10 number into base 2
  * by recursivily do integer divisions by 2 and adding the last "quotient" (1)
- * to vector The values "rests" and the last "quotient" are added through insert
- * method
+ * to vector. The values "rests" and the last "quotient" are added through
+ * insert method.
+ *
+ * TODO: Excuse-me sir 🧐, do you think I have a shitload of RAM?
+ * BECAUSE I DON'T!!! This recursive function looks cool but if the input is too
+ * much then when are adding a bunch of stuff to stack. Maybe I should've used a
+ * for loop instead. I don't know, I will make some tests...
  */
 
 void base10to2(int base10, std::vector<int> &v_base2) {
